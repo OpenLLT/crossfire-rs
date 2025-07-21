fb65378f5e3382c3dc2db4ee4edda54eb92d920c
plan <frostyplanet@gmail.com>
2025-07-21 22:30:36 +0800

    Remove send_waker_done()
    There's no chance calling this.
    Those already waked is not in registry.
    
    Cancel send waker in try_send_with_lock(), good for spsc.
    
    crossfire_bounded_blocking_1_1/spsc 1x1 size 1
    	thrpt: 3.6414 Melem/s  +27%

--- src/blocking_tx.rs
+++ src/blocking_tx.rs
@@ -114,7 +114,6 @@ impl<T: Send + 'static> Tx<T> {
                 macro_rules! process_state {
                     ($state: expr) => {
                         if $state == WakerState::DONE as u8 {
-                            shared.send_waker_done(&waker);
                             self.waker_cache.push(waker);
                             return Ok(());
                         } else if $state == WakerState::CLOSED as u8 {
@@ -154,7 +153,6 @@ impl<T: Send + 'static> Tx<T> {
                     if state == WakerState::WAKED as u8 {
                         if shared.try_send(&_item).is_ok() {
                             shared.on_send();
-                            shared.send_waker_done(&waker);
                             self.waker_cache.push(waker);
                             tx_stats!(_i, true);
                             return Ok(());
