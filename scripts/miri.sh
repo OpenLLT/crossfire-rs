8659c3bcc2ff8abde00379479288d7a74b34f9f4
plan <frostyplanet@gmail.com>
2025-09-14 03:58:07 +0800

    turn on tokio log

--- scripts/miri.sh
+++ scripts/miri.sh
@@ -1,2 +1,3 @@
 #MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-backtrace=full -Zmiri-deterministic-concurrency -Zmiri-tree-borrows -Zmiri-strict-provenance " cargo +nightly miri test -- --no-capture --test-threads=1
-MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full" cargo +nightly miri test $@ --features trace_log -- --no-capture --test-threads=1
+#MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full" cargo +nightly miri test $@ --features trace_log -- --no-capture --test-threads=1
+RUSTFLAGS="--cfg tokio_unstable" MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full" cargo +nightly miri test $@ --features trace_log -- --no-capture --test-threads=1
