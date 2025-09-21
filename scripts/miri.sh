f95a37235d67d95ea347eb932afd684355cd2f13
plan <frostyplanet@gmail.com>
2025-09-21 11:18:21 +0800

    miri.sh: allow passing in MIRIFLAGS

--- scripts/miri.sh
+++ scripts/miri.sh
@@ -3,4 +3,7 @@
 # -Zmiri-permissive-provenance is to disable warning about parking_lot
 
 # By default log is off, if you need to enable, pass the option with the script: --features trace_log
-RUSTFLAGS="--cfg tokio_unstable" RUST_BACKTRACE=1 MIRIFLAGS="-Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full -Zmiri-permissive-provenance" cargo +nightly miri test $@ -- --no-capture --test-threads=1
+
+MIRIFLAGS="$MIRIFLAGS -Zmiri-disable-isolation -Zmiri-no-short-fd-operations -Zmiri-backtrace=full -Zmiri-permissive-provenance"
+export MIRIFLAGS
+RUSTFLAGS="--cfg tokio_unstable" RUST_BACKTRACE=1 cargo +nightly miri test $@ -- --no-capture --test-threads=1
