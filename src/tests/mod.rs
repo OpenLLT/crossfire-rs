22644926426b2cbb2d076b249b0a6cb44e6ac005
plan <frostyplanet@gmail.com>
2025-10-12 23:15:44 +0800

    delete test with timeout for tokio debuging

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,10 +1,3 @@
 mod common;
 mod test_async;
-mod test_async_blocking;
-mod test_blocking_async;
-#[allow(unused_imports)]
 mod test_blocking_context;
-
-// we don't want to import smol-timeout
-#[cfg(not(feature = "smol"))]
-mod test_type_switch;
